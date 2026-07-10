a300511e4343d17603efae1810202e76eadaa872
kurniawanrizzki <kurniawanrizzki@gmail.com>
2026-07-10 09:16:31 +0700

    Use method-prefix constants in router; add sonar config

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -11,6 +11,14 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// HTTP method prefixes used in ServeMux route patterns ("METHOD /path").
+const (
+	methodPost   = http.MethodPost + " "
+	methodGet    = http.MethodGet + " "
+	methodPut    = http.MethodPut + " "
+	methodDelete = http.MethodDelete + " "
+)
+
 type rest struct {
 	mux  *http.ServeMux
 	mw   middleware.Middleware
@@ -38,20 +46,20 @@ func InitHttpHandler(mux *http.ServeMux, mw middleware.Middleware, svc *service.
 func (e *rest) Serve() {
 	handler := e.mw.Handler()
 
-	e.mux.Handle("POST "+preference.RouteCreateCollege, handler(http.HandlerFunc(e.CreateCollege)))
-	e.mux.Handle("GET "+preference.RouteGetColleges, handler(http.HandlerFunc(e.FindAll)))
-	e.mux.Handle("GET "+preference.RouteCollegeByNIM, handler(http.HandlerFunc(e.FindByNim)))
-	e.mux.Handle("GET "+preference.RouteCollegeByName, handler(http.HandlerFunc(e.FindByName)))
-	e.mux.Handle("GET "+preference.RouteCollegeBySemester, handler(http.HandlerFunc(e.FindBySemester)))
-	e.mux.Handle("PUT "+preference.RouteCollegeByNIM, handler(http.HandlerFunc(e.UpdateCollege)))
-	e.mux.Handle("DELETE "+preference.RouteCollegeByNIM, handler(http.HandlerFunc(e.DeleteCollege)))
-	e.mux.Handle("POST "+preference.RouteCreateCourse, handler(http.HandlerFunc(e.CreateCourse)))
-	e.mux.Handle("GET "+preference.RouteGetCourses, handler(http.HandlerFunc(e.GetAllCourses)))
-	e.mux.Handle("GET "+preference.RouteCourseByCode, handler(http.HandlerFunc(e.GetCourseByCode)))
-	e.mux.Handle("PUT "+preference.RouteCourseByCode, handler(http.HandlerFunc(e.UpdateCourse)))
-	e.mux.Handle("DELETE "+preference.RouteCourseByCode, handler(http.HandlerFunc(e.DeleteCourse)))
-	e.mux.Handle("POST "+preference.RouteCreateEnrollment, handler(http.HandlerFunc(e.CreateEnrollment)))
-	e.mux.Handle("GET "+preference.RouteEnrollmentByNIM, handler(http.HandlerFunc(e.GetEnrollmentsByNim)))
-	e.mux.Handle("PUT "+preference.RouteEnrollmentByNimCode, handler(http.HandlerFunc(e.UpdateEnrollment)))
-	e.mux.Handle("DELETE "+preference.RouteEnrollmentByID, handler(http.HandlerFunc(e.DeleteEnrollment)))
+	e.mux.Handle(methodPost+preference.RouteCreateCollege, handler(http.HandlerFunc(e.CreateCollege)))
+	e.mux.Handle(methodGet+preference.RouteGetColleges, handler(http.HandlerFunc(e.FindAll)))
+	e.mux.Handle(methodGet+preference.RouteCollegeByNIM, handler(http.HandlerFunc(e.FindByNim)))
+	e.mux.Handle(methodGet+preference.RouteCollegeByName, handler(http.HandlerFunc(e.FindByName)))
+	e.mux.Handle(methodGet+preference.RouteCollegeBySemester, handler(http.HandlerFunc(e.FindBySemester)))
+	e.mux.Handle(methodPut+preference.RouteCollegeByNIM, handler(http.HandlerFunc(e.UpdateCollege)))
+	e.mux.Handle(methodDelete+preference.RouteCollegeByNIM, handler(http.HandlerFunc(e.DeleteCollege)))
+	e.mux.Handle(methodPost+preference.RouteCreateCourse, handler(http.HandlerFunc(e.CreateCourse)))
+	e.mux.Handle(methodGet+preference.RouteGetCourses, handler(http.HandlerFunc(e.GetAllCourses)))
+	e.mux.Handle(methodGet+preference.RouteCourseByCode, handler(http.HandlerFunc(e.GetCourseByCode)))
+	e.mux.Handle(methodPut+preference.RouteCourseByCode, handler(http.HandlerFunc(e.UpdateCourse)))
+	e.mux.Handle(methodDelete+preference.RouteCourseByCode, handler(http.HandlerFunc(e.DeleteCourse)))
+	e.mux.Handle(methodPost+preference.RouteCreateEnrollment, handler(http.HandlerFunc(e.CreateEnrollment)))
+	e.mux.Handle(methodGet+preference.RouteEnrollmentByNIM, handler(http.HandlerFunc(e.GetEnrollmentsByNim)))
+	e.mux.Handle(methodPut+preference.RouteEnrollmentByNimCode, handler(http.HandlerFunc(e.UpdateEnrollment)))
+	e.mux.Handle(methodDelete+preference.RouteEnrollmentByID, handler(http.HandlerFunc(e.DeleteEnrollment)))
 }
